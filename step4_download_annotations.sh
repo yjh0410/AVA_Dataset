8535bf6ec76c93d47958a695a6367c67cf0da8b3
yjh0410 <1394571815@qq.com>
2022-08-16 14:19:11 +0800

    update

--- step4_download_annotations.sh
+++ step4_download_annotations.sh
@@ -10,9 +10,17 @@ wget https://dl.fbaipublicfiles.com/video-long-term-feature-banks/data/ava/frame
 cd ..
 cd annotations
 
-wget https://s3.amazonaws.com/ava-dataset/annotations/ava_v2.2.zip
 wget https://dl.fbaipublicfiles.com/video-long-term-feature-banks/data/ava/annotations/ava_train_predicted_boxes.csv
 wget https://dl.fbaipublicfiles.com/video-long-term-feature-banks/data/ava/annotations/ava_val_predicted_boxes.csv
 wget https://dl.fbaipublicfiles.com/video-long-term-feature-banks/data/ava/annotations/ava_test_predicted_boxes.csv
 
+mkdir ava_v2.2
+cd ava_v2.2  # -> annotations/ava_v2.2
+wget https://s3.amazonaws.com/ava-dataset/annotations/ava_v2.2.zip
 unzip ava_v2.2.zip
+
+cd ..  # -> annotations/
+mkdir ava_v2.1
+cd ava_v2.1  # -> annotations/ava_v2.1
+wget https://s3.amazonaws.com/ava-dataset/annotations/ava_v2.1.zip
+unzip ava_v2.1.zip
